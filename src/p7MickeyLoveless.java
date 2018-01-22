d3c39e13dccddc59eed26e2b11ff7718b95517a1
ml0981781 <ml0981781@RK03-B1225-05.francistuttle.edu>
2018-01-22 09:38:14 -0600

    First version

---
+++ src/p7MickeyLoveless.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class p7MickeyLoveless
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter the length of the array --> ");
+		int[] list = new int[input.nextInt()];
+		
+		fill(list);
+		
+		System.out.println("The average is " + average(list));
+		
+		show(list);
+	}
+	
+	public static void fill(int[] arr)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter your numbers --> ");
+		for(int x = 0; x < arr.length; ++x)
+		{
+			arr[x] = input.nextInt();
+		}
+	}
+	
+	public static double average(int[] arr)
+	{
+		double sum = 0;
+		
+		for(int x: arr)
+		{
+			sum += x;
+		}
+		
+		return (double) (sum/arr.length);
+	}
+	
+	public static void show(int[] arr)
+	{
+		System.out.print("The whole array is ");
+		for(int x: arr)
+		{
+			System.out.print(x + " ");
+		}
+	}
+}
